2e056d12b50631b15506f87110743c1a148cb7e3
mohosin16103308 <hamza1610330816@gmail.com>
2022-07-02 15:16:09 +0600

    React Datatable Filter Added

--- src/pages/property/PropertyList.js
+++ src/pages/property/PropertyList.js
@@ -1,13 +1,65 @@
 
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import DataTable from 'react-data-table-component';
+import styled from 'styled-components';
 import { DashboardLayout } from '../../components/Layout';
 import './PropertyList.css';
 
-const PropertyList = () => {
+import Button from '@mui/material/Button';
+    
+  const TextField = styled.input`
+  	height: 32px;
+  	width: 200px;
+  	border-radius: 3px;
+  	border-top-left-radius: 5px;
+  	border-bottom-left-radius: 5px;
+  	border-top-right-radius: 0;
+  	border-bottom-right-radius: 0;
+  	border: 1px solid #e5e5e5;
+  	padding: 0 32px 0 16px;
+  
+  	&:hover {
+  		cursor: pointer;
+  	}
+  `;
+  
+  const ClearButton = styled(Button)`
+  	border-top-left-radius: 0;
+  	border-bottom-left-radius: 0;
+  	border-top-right-radius: 5px;
+  	border-bottom-right-radius: 5px;
+  	height: 34px;
+  	width: 32px;
+  	text-align: center;
+  	display: flex;
+  	align-items: center;
+  	justify-content: center;
+  `;
+  
+  const FilterComponent = ({ filterText, onFilter, onClear }) => (
+  	<>
+  		<TextField
+  			id="search"
+  			type="text"
+  			placeholder="Filter By Name"
+  			aria-label="Search Input"
+  			value={filterText}
+  			onChange={onFilter}
+  		/>
+  		<ClearButton type="button" onClick={onClear}>
+  			X
+		</ClearButton>
+  	</>
+  );
+  
+    
+  
+   export const Filtering = () => {
+  	const [filterText, setFilterText] = React.useState('');
+  	const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
 
-  const [properties, setProperties] = useState([]);
+    const [properties, setProperties] = useState([]);
 
   const columns = [
     {
@@ -37,19 +89,42 @@ const data = properties;
       });
 
     }
+    
+  	const filteredItems = data.filter(
+  		item => item.name && item.name.toLowerCase().includes(filterText.toLowerCase()),
+  	);
+  
+  	const subHeaderComponentMemo = React.useMemo(() => {
+  		const handleClear = () => {
+  			if (filterText) {
+  				setResetPaginationToggle(!resetPaginationToggle);
+  				setFilterText('');
+  			}
+  		};
+  
+  		return (
+  			<FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} />
+  		);
+  	}, [filterText, resetPaginationToggle]);
+  
+  	return (
+      <DashboardLayout>
+  		<DataTable
+  			title="Contact List"
+  			columns={columns}
+  			data={filteredItems}
+  			pagination
+  			paginationResetDefaultPage={resetPaginationToggle} // optionally, a hook to reset pagination to page 1
+  			subHeader
+  			subHeaderComponent={subHeaderComponentMemo}
+  			selectableRows
+  			persistTableHead
+  		/>
+      </DashboardLayout>
+	);
+  };
+  
+   
+ 
 
-
-  return (
-    <DashboardLayout>
-      <h2>PropertyList</h2>
-      <DataTable
-        columns={columns}
-        data={data}
-        pagination
-      />
-      
-    </DashboardLayout>
-  )
-}
-
-export default PropertyList;
+export default Filtering;
--- src/pages/property/add/PropertyAdd.js
+++ src/pages/property/add/PropertyAdd.js
@@ -1,115 +1,70 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import axios from 'axios';
 import { useState } from 'react';
+import swal from 'sweetalert';
 import { DashboardLayout } from "../../../components/Layout";
 
 const PropertyAdd = () => {
 
-	const [name, setName] = useState();
-	const [code, setCode] = useState();
-	const [type, setType] = useState();
-	const [rent_amount, setRentAmount] = useState();
-	const [size, setSize] = useState();
-		
-	async function loginUser(credentials) {
-		const token = localStorage.getItem('access_token');
-		return fetch('https://faptl.americanbestit.com/api/v1/properties', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization' : `Bearer ${token}`
 
 
-		},
-		body: JSON.stringify(credentials)
-		})
-		.then(data => data.json())
-	}
+    const [formValue, setformValue] = useState({
+        name: '',
+        code: '',
+        type: '',
+        address: '',
+        city: '',
+        state: '',
+        zip: '',
+        note: '',
+        rent_amount: '',
+        size: '',
+        link: '',
+        has_parking: '',
+        has_security_gard: '',
+        isActive: '',
+      });
 
-		const handleSubmit = async e => {
+	  const handleSubmit = async(e) => {
+		// store the states in the form data
 		e.preventDefault();
-		const response = await loginUser({
-			name,
-			code,
-			type,
-			rent_amount,
-			size
-		});
+		const loginFormData = new FormData();
+		loginFormData.append("name", formValue.name)
+		loginFormData.append("code", formValue.code)
+		loginFormData.append("rent_amount", formValue.rent_amount)
+		loginFormData.append("size", formValue.size)
+		
+		const token = localStorage.getItem('access_token');
+		let errors = null;
+		try {
+		  // make axios post request
+		  const response = await axios({
+			method: 'POST',
+			url: 'https://faptl.americanbestit.com/api/v1/properties',
+			data: loginFormData,
+			headers: { 'Content-Type': 'multipart/form-data' , 'Authorization' : `Bearer ${token}` },
+		  });
+		 console.log( response );
+			swal("Success", "Property Added Successfully", "success", {
+				buttons: false,
+				timer: 20454500,
+				});
 
-	
-	}
-
-
-
-	// const [formValue, setformValue] = useState({
-	// 	name: '',
-	// 	code: '',
-	// 	type: '',
-	// 	address: '',
-	// 	city: '',
-	// 	state: '',
-	// 	zip: '',
-	// 	note: '',
-	// 	rent_amount: '',
-	// 	size: '',
-	// 	link: '',
-	// 	has_parking: '',
-	// 	has_security_gard: '',
-	// 	isActive: '',
-	// });
-
-	// const handleSubmit = async (event) => {
-	// event.preventDefault();
-	
-	// 	const token = localStorage.getItem('access_token');
-	// 	const api = 'https://faptl.americanbestit.com/api/v1/properties'; 
-
-
-	// 	// store the states in the form data
-	// 	const loginFormData = new FormData();
-	// 	loginFormData.append("name", formValue.name)
-	// 	loginFormData.append("code", formValue.code)
-	// 	loginFormData.append("type", formValue.type)
-	// 	loginFormData.append("address", formValue.address)
-	// 	loginFormData.append("city", formValue.city)
-	// 	loginFormData.append("state", formValue.state)
-	// 	loginFormData.append("zip", formValue.zip)
-	// 	loginFormData.append("note", formValue.note)
-	// 	loginFormData.append("rent_amount", formValue.rent_amount)
-	// 	loginFormData.append("size", formValue.size)
-	// 	loginFormData.append("link", formValue.link)
-	// 	loginFormData.append("has_parking", formValue.has_parking)
-	// 	try {
-	// 		console.log(loginFormData);
-	// 	// make axios post request
-	// 	const response = await axios({
-	// 		method: "post",
-	// 		url: api,
-	// 		body: loginFormData,
-	// 		headers: {"Authorization" : `Bearer ${token}`}
-	// 	});
-	// 		console.log( response );
-	// 	swal("Success", "You Logged In", "success", {
-	// 		buttons: false,
-	// 		timer: 2024343000,
-	// 		})
-
-	// 	//    window.location.href = "/property/add";
-
-	// 	} catch(error) {
-	// 	console.log(error);
-	// 	swal("Failed", 'Error', "error");
-
-	// 	}
-
-	// }
-	
-	// const handleChange = (event) => {
-	// 	setformValue({
-	// 	...formValue,
-	// 	[event.target.name]: event.target.value
-	// 	});
-	// }
+		} catch(error) {
+			
+			swal("Failed", "Please Enter Required Field Data, Code name would be Unique", "error");
+		  console.log(error);
+		 
+		}
+	  }
+    
+      const handleChange = (event) => {
+        setformValue({
+          ...formValue,
+          [event.target.name]: event.target.value
+        });
+      }
 
 
     //   const addProduct = () => {
@@ -145,72 +100,156 @@ const PropertyAdd = () => {
   return (
     <DashboardLayout>
       <h2>Property Add</h2>
-      <form
-        noValidate
-		onSubmit={handleSubmit}
-	>
-		<TextField
-			variant="outlined"
-			margin="normal"
-			required
-			fullWidth
-			id="name"
-			name="name"
-			label="Property Name"
-			onChange={e => setName(e.target.value)}
-		/>
-		<TextField
-			variant="outlined"
-			margin="normal"
-			required
-			fullWidth
-			id="code"
-			name="code"
-			label="Property Code"
-			onChange={e => setCode(e.target.value)}
-		/>
-		<TextField
-			variant="outlined"
-			margin="normal"
-			fullWidth
-			id="type"
-			name="type"
-			label="type"
-			onChange={e => setType(e.target.type)}
+			<form
+			noValidate
+			onSubmit={handleSubmit}
+			>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="name"
+					name="name"
+					type="text"
+					label="Property Name"
+					onChange={handleChange}
+				/>
 
-		/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="code"
+					name="code"
+					label="code"
+					onChange={handleChange}
+				/>
 
-		<TextField
-			variant="outlined"
-			margin="normal"
-			fullWidth
-			type="number"
-			id="rent_amount"
-			name="rent_amount"
-			label="rent_amount"
-			onChange={e => setRentAmount(e.target.rent_amount)}
 
-		/>
-		<TextField
-			variant="outlined"
-			margin="normal"
-			fullWidth
-			type="number"
-			id="size"
-			name="size"
-			label="size"
-			onChange={e => setSize(e.target.size)}
-		/>
-		
-		<Button
-			type="submit"
-			fullWidth
-			variant="contained"
-			color="primary"
-		>
-			Sign In
-		</Button>
-	</form>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="type"
+					name="type"
+					label="type"
+					type="text"
+					onChange={handleChange}
+				/>
+
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="address"
+					name="address"
+					label="address"
+					type="text"
+					onChange={handleChange}
+				/>
+
+
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="city"
+					name="city"
+					label="city"
+					type="text"
+					onChange={handleChange}
+				/>
+
+
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="state"
+					name="state"
+					label="state"
+					type="text"
+					onChange={handleChange}
+				/>
+
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="zip"
+					name="zip"
+					label="zip"
+					type="text"
+					onChange={handleChange}
+				/>
+
+
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="note"
+					name="note"
+					label="note"
+					type="text"
+					onChange={handleChange}
+				/>
+
+
+				
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="rent_amount"
+					name="rent_amount"
+					label="rent_amount"
+					type="number"
+					onChange={handleChange}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="size"
+					name="size"
+					label="size"
+					type="number"
+					onChange={handleChange}
+				/>
+
+
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="link"
+					name="link"
+					label="link"
+					type="text"
+					onChange={handleChange}
+				/>
+
+				<Button
+				type="submit"
+				fullWidth
+				variant="contained"
+				color="primary"
+				>
+				Sign In
+				</Button>
+            </form>
 
  
     </DashboardLayout>
