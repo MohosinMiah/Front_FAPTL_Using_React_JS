a2751f822cdf09e797e94d4dd03ee31977770075
mohosin16103308 <hamza1610330816@gmail.com>
2022-07-14 11:45:28 +0600

    Property Unit Added Successfull

--- src/pages/propertyunit/add/PropertyUnitAdd.js
+++ src/pages/propertyunit/add/PropertyUnitAdd.js
@@ -9,14 +9,44 @@ import './PropertyUnitAdd';
 const PropertyAdd =  () => {
 	let { id } = useParams();
 
+	// Selected Option Dropdown Types
+	const types = [
+		{value: '', text: '--Select Type--'},
+		{value: 'Residential', text: 'Residential'},
+		{value: 'Business', text: 'Business'},
+	  ];
 
+	const unitTypes = [
+		{value: '', text: '--Select Unit Type--'},
+		{value: 'Normal', text: 'Normal'},
+		{value: 'Studio', text: 'Studio'},
+		{value: 'Duplex', text: 'Duplex'},
+		];
+
+	const isAvailables = [
+		{value: '', text: '--Select Is Available--'},
+		{value: '1', text: 'Yes'},
+		{value: '0', text: 'No'},
+		];
+
+	const isFeatureds = [
+		{value: '', text: '--Select Is Featured--'},
+		{value: '1', text: 'Yes'},
+		{value: '0', text: 'No'},
+		];
+	
+	const isActives = [
+		{value: '', text: '--Select Is Active--'},
+		{value: '1', text: 'Yes'},
+		{value: '0', text: 'No'},
+		];
+	// All Property Unit Variable initialized
 	const [ property_id, setPropertyID ] = useState( id );
-	const [ type, setType ] = useState('');
+	const [ type, setType ] = useState(types[0].value);
 	const [ name, setName ] = useState('');
 	const [ floor, setFloor ] = useState('');
 	const [ rent, setRent ] = useState('');
-	const [ unit_type, setUnitType ] = useState('');
-	const [ zip, setZip ] = useState('');
+	const [ unit_type, setUnitType ] = useState(unitTypes[0].value);
 	const [ size, setSize ] = useState('');
 	const [ total_room, setTotalRoom ] = useState('');
 	const [ bed_room, setBedRoom ] = useState( '' );
@@ -29,10 +59,9 @@ const PropertyAdd =  () => {
 
 
 
+	// store the states in the form data
 	  const handleSubmit = async(e) => {
-		// store the states in the form data
 		e.preventDefault();
-
 		try {
 			addPropertyUnit();
 
@@ -42,7 +71,7 @@ const PropertyAdd =  () => {
 	
 	  }
     
-
+	// Handle Submotted data
       const addPropertyUnit = () => {
 
         const api = 'https://faptl.americanbestit.com/api/v1/propertyunits'; 
@@ -96,11 +125,17 @@ const PropertyAdd =  () => {
 				<form noValidate onSubmit={handleSubmit}>
 					<div className="form-outline">
 						<label className="form-label">Property ID <sup>*</sup></label>
-						<input type="text" name="property_id" className="form-control" value={property_id}  readonly />
+						<input type="text" name="property_id" className="form-control" value={property_id}  readOnly />
 					</div>
 					<div className="form-outline">
 						<label className="form-label">Type<sup>*</sup></label>
-						<input type="text" name="type" className="form-control" onChange={e => setType(e.target.value)} />
+						<select  name="type" className="form-control"  value={type} onChange={e => setType(e.target.value)}>
+							{types.map(option => (
+							<option key={option.value} value={option.value}>
+								{option.text}
+							</option>
+							))}
+						</select>
 					</div>
 					<div className="form-outline">
 						<label className="form-label">Name</label>
@@ -108,7 +143,7 @@ const PropertyAdd =  () => {
 					</div>
 					<div className="form-outline">
 						<label className="form-label">Floor<sup>*</sup></label>
-						<input type="text" name="floor" className="form-control" onChange={e => setFloor(e.target.value)} />
+						<input type="number" name="floor" className="form-control" onChange={e => setFloor(e.target.value)} />
 					</div>
 					<div className="form-outline">
 						<label className="form-label">Rent Amount<sup>*</sup></label>
@@ -116,7 +151,14 @@ const PropertyAdd =  () => {
 					</div>
 					<div className="form-outline">
 						<label className="form-label">Unit Type<sup>*</sup></label>
-						<input type="text" name="unit_type" className="form-control" onChange={e => setUnitType(e.target.value)} />
+						<select  name="unit_type"  value={unit_type} onChange={e => setUnitType(e.target.value)} required>
+							{unitTypes.map(option => (
+							<option key={option.value} value={option.value}>
+								{option.text}
+							</option>
+							))}
+						</select>
+
 					</div>
 					<div className="form-outline">
 						<label className="form-label">Size<sup>*</sup></label>
@@ -144,17 +186,32 @@ const PropertyAdd =  () => {
 					</div>
 					<div className="form-outline">
 						<label className="form-label">Is Available</label>
-						<input type="text" name="isAvailable" className="form-control" onChange={e => setIsAvailable(e.target.value)} />
+						<select  name="isAvailable" className="form-control"  value={isAvailable} onChange={e => setIsAvailable(e.target.value)}>
+							{isAvailables.map(option => (
+							<option key={isAvailables.value} value={option.value}>
+								{option.text}
+							</option>
+							))}
+						</select>
 					</div>
 					<div className="form-outline">
 						<label className="form-label">Is Featured</label>
-						<input type="text" name="isFeatured" className="form-control" onChange={e => setIsFeatured(e.target.value)} />
+						<select  name="isFeatured" className="form-control"  value={isFeatured} onChange={e => setIsFeatured(e.target.value)}>
+							{isFeatureds.map(option => (
+							<option key={option.value} value={option.value}>
+								{option.text}
+							</option>
+							))}
+						</select>
 					</div>
 					<div className="form-outline">
 						<label className="form-label">Is Active</label>
-						<select class="form-control"name="isActive" className="form-control" onChange={e => setIsActive(e.target.value)} >
-							<option value="1">Yes</option>
-							<option value="0">No</option>
+						<select  name="isActive" className="form-control"  value={isActive} onChange={e => setIsActive(e.target.value)}>
+							{isActives.map(option => (
+							<option key={option.value} value={option.value}>
+								{option.text}
+							</option>
+							))}
 						</select>
 					</div>
 					
